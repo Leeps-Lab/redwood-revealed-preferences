4a075bcdb9de51829a1ca816e54b1a3390eb1acd
Matthew Jee <matthew@marmphco.com>
2014-12-12 14:05:24 -0800

    added cursor labels

--- plot.js
+++ plot.js
@@ -13,28 +13,34 @@ Redwood.directive("rpPlot", function ($compile) {
             width: "=",
             height: "=",
          },
-         template: '<svg width="{{width}}" height="{{height}}"></svg>',
+         template: '<svg width="{{width}}" height="{{height}}"></svg>\
+                    <div class="endowment-label">{{endowment.x | number: 2}}, {{endowment.y | number: 2}}</div>\
+                    <div class="selection-label">{{selection[0] | number: 2}}, {{selection[1] | number: 2}}</div>\
+                    <div class="hover-label x">{{cursor[0] | number: 2}}</div>\
+                    <div class="hover-label y">{{cursor[1] | number: 2}}</div>',
          link: function ($scope, $elem, attr) {
 
             var svg = d3.select($elem[0]).select("svg");
 
-            var xOffset = 100;
+            var xOffset = 50;
             var yOffset = 50;
             var plotWidth = $scope.width - 2 * xOffset;
-            var plotHeight = $scope.height - 1.5 * yOffset;
+            var plotHeight = $scope.height - 2 * yOffset;
 
-            var clipPath = svg.append("clipPath")
-                .attr("id", "clipPath");
-            clipPath.append("rect")
-                .attr("width", plotWidth)
-                .attr("height", plotHeight);
+            svg.append("clipPath")
+                .attr("id", "clipPath")
+                .append("rect")
+                    .attr("width", plotWidth)
+                    .attr("height", plotHeight);
+
+            var axes = svg.append("g");
 
             var plot = svg.append("g")
-                .attr("transform", "translate(" + xOffset + "," + (yOffset/2) + ")")
+                .attr("transform", "translate(" + xOffset + "," + yOffset + ")")
                 .attr("clip-path", "url(#clipPath)");
 
             plot.append("rect")
-                .attr("fill", "#dddddd")
+                .classed("plot-background", true)
                 .attr("width", plotWidth)
                 .attr("height", plotHeight);
 
@@ -63,38 +69,33 @@ Redwood.directive("rpPlot", function ($compile) {
                     .scale(yScale)
                     .orient("left");
 
-                svg.select("g.x.axis").remove();
-                svg.select("g.y.axis").remove();
+                axes.select("g.x.axis").remove();
+                axes.select("g.y.axis").remove();
 
-                svg.append("g")
+                axes.append("g")
                     .classed("x axis", true)
-                    .attr("transform", "translate(" + xOffset + "," + (plotHeight+yOffset/2) + ")")
+                    .attr("transform", "translate(" + xOffset + "," + (plotHeight+yOffset) + ")")
                     .call(xAxis)
                     .append("text")
+                        .classed("axis-label", true)
                         .attr("text-anchor", "middle")
                         .attr("x", plotWidth/2)
                         .attr("y", yOffset - 5)
-                        .text("Amount of X [Probability of X: " + $scope.probX.toFixed(2) + "]");
-                svg.append("g")
+                        .text("Amount of X (Probability: " + $scope.probX.toFixed(2) + ")");
+                axes.append("g")
                     .classed("y axis", true)
-                    .attr("transform", "translate(" + xOffset + "," + (yOffset/2) + ")")
+                    .attr("transform", "translate(" + xOffset + "," + yOffset + ")")
                     .call(yAxis)
                     .append("text")
+                        .classed("axis-label", true)
                         .attr("text-anchor", "middle")
                         .attr("transform", "rotate(-90)")
                         .attr("x", -plotHeight/2)
                         .attr("y", -xOffset + 10)
-                        .text("Amount of Y [Probability of Y: " + (1-$scope.probX).toFixed(2) + "]");
+                        .text("Amount of Y (Probability: " + (1-$scope.probX).toFixed(2) + ")");
             }
 
-            var drawBudgetLine = function () {
-                if (!$scope.budgetFunc) return;
-
-                var coordinates = [
-                    [0, $scope.budgetFunc(0)],
-                    [$scope.inverseBudgetFunc(0), 0]
-                ];
-
+            var drawLine = function (coordinates, className) {
                 var lineFunction = d3.svg.line()
                     .x(function(d) {
                         return xScale(d[0])
@@ -103,11 +104,11 @@ Redwood.directive("rpPlot", function ($compile) {
                         return yScale(d[1])
                     })
                     
-                var path = plot.select(".budget-line");
+                var path = plot.select("." + className);
                 if (path.empty()) {
                     plot.append("path")
                         .datum(coordinates)
-                        .classed("budget-line", true)
+                        .classed(className, true)
                         .attr("d", lineFunction);
                 } else {
                     path.datum(coordinates).attr("d", lineFunction);
@@ -132,17 +133,53 @@ Redwood.directive("rpPlot", function ($compile) {
                     });
             }
 
-            var drawEndowment = function() {
+            var drawBudgetLine = function () {
+                if (!$scope.budgetFunc) return;
+                var coordinates = [
+                    [0, $scope.budgetFunc(0)],
+                    [$scope.inverseBudgetFunc(0), 0]
+                ];
+                drawLine(coordinates, "budget-line");
+            }
+
+            var drawEndowment = function () {
                 if (!$scope.endowment) return;
                 drawPoint([$scope.endowment.x, $scope.endowment.y], "endowment-point");
             }
 
-            var drawCursor = function() {
-                if (!$scope.cursor) return;
+            var drawCursor = function () {
+                if (!$scope.cursor) {
+                    d3.select(".cursor-point").remove();
+                    d3.select(".cursor-crosshair-x").remove();
+                    d3.select(".cursor-crosshair-y").remove();
+                    d3.selectAll(".hover-label").style("display", "none");
+                    return;
+                }
                 drawPoint($scope.cursor, "cursor-point");
+                drawLine([$scope.cursor, [$scope.cursor[0], 0]], "cursor-crosshair-x");
+                drawLine([$scope.cursor, [0, $scope.cursor[1]]], "cursor-crosshair-y");
+
+                // draw hover labels
+                var plotRect = d3.select(".plot-background")[0][0].getBoundingClientRect();
+                var xLabelRect = d3.select(".hover-label.x")[0][0].getBoundingClientRect();
+                var yLabelRect = d3.select(".hover-label.y")[0][0].getBoundingClientRect();
+                d3.selectAll(".hover-label").style("display", "block");
+                d3.select(".hover-label.x")
+                    .style({
+                        "position": "fixed",
+                        "top": (plotRect.bottom+yOffset).toString() + "px",
+                        "left": (plotRect.left+xScale($scope.cursor[0])-xLabelRect.width/2).toString() + "px"
+                    });
+                d3.select(".hover-label.y")
+                    .style({
+                        "position": "fixed",
+                        "width": "100px",
+                        "top": (plotRect.top+plotHeight-xScale($scope.cursor[1])-yLabelRect.height/2).toString() + "px",
+                        "left": (plotRect.left-xOffset*3).toString() + "px"
+                    });
             }
 
-            var drawSelection = function() {
+            var drawSelection = function () {
                 if (!$scope.selection) return;
                 drawPoint($scope.selection, "selection-point");
             }
@@ -158,12 +195,12 @@ Redwood.directive("rpPlot", function ($compile) {
 
             var redraw = function () {
                 buildScales();
+                drawAxes();
                 drawBudgetLine();
                 drawEndowment();
                 drawSelection();
                 drawCursor();
                 drawResult();
-                drawAxes();
             }
 
             $scope.$watchCollection("limits", redraw);
@@ -171,39 +208,28 @@ Redwood.directive("rpPlot", function ($compile) {
             $scope.$watch("selection", drawSelection);
             $scope.$watch("result", drawResult);
 
-                $(plot).popover({
-                    trigger: 'hover',
-                    content: '12',
-                    container: 'body'
-                });
-
-            plot.on("click", function() {
+            svg.on("click", function() {
                 if (!$scope.inputEnabled) return;
-
-                var xMouse = xScale.invert(d3.mouse(this)[0]);
-                var xMin = Math.max(0, $scope.inverseBudgetFunc($scope.limits.x));
-                var xMax = Math.min($scope.limits.x, $scope.inverseBudgetFunc(0))
-                var x = Math.min(xMax, Math.max(xMin, xMouse));
-
-                $scope.cursor = [x, $scope.budgetFunc(x)];
                 $scope.$emit("rpPlot.click", $scope.cursor);
                 drawSelection();
             });
 
-            plot.on("mousemove", function() {
+            svg.on("mousemove", function() {
                 if (!$scope.inputEnabled) return;
 
-                var xMouse = xScale.invert(d3.mouse(this)[0]);
+                var xMouse = xScale.invert(d3.mouse(plot[0][0])[0]);
                 var xMin = Math.max(0, $scope.inverseBudgetFunc($scope.limits.x));
                 var xMax = Math.min($scope.limits.x, $scope.inverseBudgetFunc(0))
                 var x = Math.min(xMax, Math.max(xMin, xMouse));
 
-                $scope.cursor = [x, $scope.budgetFunc(x)];
+                $scope.$apply(function () {
+                    $scope.cursor = [x, $scope.budgetFunc(x)];
+                });
                 drawCursor();
             });
 
-            plot.on("mouseout", function() {
-                $scope.cursor = [$scope.endowment.x, $scope.endowment.y];
+            svg.on("mouseleave", function() {
+                $scope.cursor = null;
                 drawCursor();
             });
         }
